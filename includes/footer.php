247edf592e2ad3accc2bdd52a10f2e4b4d756ea7
Wenuja Liyanamana <wenujaweb@gmail.com>
2026-08-19 18:02:51 +0530

    Add footer section with event description and community links

---
+++ includes/footer.php
@@ -0,0 +1,28 @@
+<?php include 'header.php'; ?>
+
+<footer class="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
+  <aside>
+    <img src="https://alumni.nsbm.ac.lk/members/img/1.png" alt="" width="150px">
+    <br>
+    <p>
+      <b>NSBM Green University</b>
+      <br />
+      A centralised platform where NSBM students can discover,<br> explore, and register for upcoming university events.
+    </p>
+  </aside>
+  <nav>
+    <h6 class="footer-title">Categories</h6>
+    <a href="/student/tech.php" class="link link-hover">Tech</a>
+    <a href="/student/marketing.php" class="link link-hover">Marketing</a>
+    <a href="/student/food-and-drink.php" class="link link-hover">Food & Drink</a>
+    <a href="/student/arts-and-culture.php" class="link link-hover">Arts & Culture</a>
+
+  </nav>
+  <nav>
+    <h6 class="footer-title">Communities</h6>
+    <a class="link link-hover">IEEE</a>
+    <a class="link link-hover">Rotract Club</a>
+    <a class="link link-hover">Marketing Circle</a>
+    <a class="link link-hover">Buddhist Society</a>
+  </nav>
+</footer>
--- includes/header.php
+++ includes/header.php
@@ -9,5 +9,4 @@
     <title>NSBM Event Hub</title>
 
 </head>
-
 <body>
