70d9000b422b5862a8aa8f47f0d5393c2d980060
Senuri Amarasinghe <senuri676@gmail.com>
2026-08-28 14:01:11 +0530

    Add community page for Marketing Circle with header, description, and follow button

---
+++ student/community.php
@@ -0,0 +1,32 @@
+<?php include '../includes/header.php'; ?>
+<?php include '../includes/navbar.php'; ?>
+
+<div class = "main-img">
+    <img src = "" alt = "marketing-circle-main">
+</div>
+<div class = "community-name">
+    <h3>Marketing Circle</h3>
+</div>
+<div class = "description">
+    <h4>The University Marketing Circle is a creative community where students connect, learn, and turn innovative marketing ideas into impactful campaigns and experiences.</h4>
+</div>
+<div class = "follow-button">
+    <button type = "button">Follow</button>
+</div>
+<div class = "events-list">
+    <h3>EVENTS</h3>
+    <>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include '../includes/footer.php'; ?>
